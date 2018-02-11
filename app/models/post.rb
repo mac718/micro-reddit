0ef1ee6d331c89d3576a6d9a15ae4511d663e433
mac718 <mac718@gmail.com>
2018-02-11 11:06:34 -0500

    Add Comment model and associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
  
   validates :title, presence: true, length: { maximum: 400 }
   validates :body, presence: true, length: { maximum: 5000 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
+  has_many :comments
 
   validates :username, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
